e3c3a4f06e90c852955d830d51f8a66e08a9b9e4
Efat Shikder <61145086+efat1531@users.noreply.github.com>
2023-08-24 21:35:09 +0600

    Update api_constant.dart

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -1,8 +1,8 @@
 String addVoterUserIdapi =
-    'https://online-election-system-fb9f4-default-rtdb.asia-southeast1.firebasedatabase.app/elections/';
+    'Your API Key';
 String signUp_api =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD978OZurANZGMwTazOnMB_CcENxWdoVAk';
+    'Your API Key';
 String signIn_api =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD978OZurANZGMwTazOnMB_CcENxWdoVAk';
+    'Your API Key';
 String election_api =
-    'online-election-system-fb9f4-default-rtdb.asia-southeast1.firebasedatabase.app';
+    'Your API Key';
